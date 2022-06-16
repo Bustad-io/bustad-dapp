8b5e8e0ebeb93e1e99398d6dfa1a80055aed217a
Rina Andriana <rina@andriana.no>
2022-06-16 12:15:13 +0200

    Fix governance page

--- src/pages/GovernancePage.tsx
+++ src/pages/GovernancePage.tsx
@@ -1,15 +1,18 @@
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { Minter } from "../features/minter/minter";
-import { connectWalletAsync, fetchBalanceAsync, fetchGovernanceDistributorShareAsync, selectAccount, selectWalletBalance, selectWalletGovernanceDistributionShare, selectWalletStatus } from "../features/wallet/walletSlice";
+import { connectWalletAsync, fetchBalanceAsync, fetchGovernanceDistributorShareAsync, selectWalletBalance, selectWalletGovernanceDistributionShare, selectWalletStatus } from "../features/wallet/walletSlice";
 import { useEffect } from 'react';
 import { useWeb3Connector } from "../hooks/web3Hook";
 
 function GovernancePage() {
   const walletGovernanceDistributionShare = useAppSelector(selectWalletGovernanceDistributionShare);
   const walletStatus = useAppSelector(selectWalletStatus);
-  const walletBalance = useAppSelector(selectWalletBalance);
+  const walletBalance = useAppSelector(selectWalletBalance);  
 
   const { contracts } = useWeb3Connector();
+
+  const votingPower = (walletBalance.govToken/100000000 * 100).toFixed(5);
+
+  const canClaim = walletGovernanceDistributionShare > 0;
 
   const dispatch = useAppDispatch();
 
@@ -33,10 +36,11 @@ function GovernancePage() {
   }
 
   return (
-    <div className="flex flex-col"> 
-      <span className="text-xl mb-6">Governance tokens to claim: {walletGovernanceDistributionShare}</span>
+    <div className="flex flex-col">       
       <span className="text-xl mb-6">Governance tokens balance: {walletBalance.govToken}</span>
-      <button onClick={onClickClaim} className="border bg-blue-300">Claim now!</button>
+      <span className="text-xl mb-6">Your voting power: {votingPower} %</span>
+      <span className="text-xl mb-6">Governance tokens to claim: {walletGovernanceDistributionShare}</span>
+      <button disabled={!canClaim} onClick={onClickClaim} className="border bg-blue-300 disabled:opacity-40">Claim now!</button>
     </div>
   );
 }
