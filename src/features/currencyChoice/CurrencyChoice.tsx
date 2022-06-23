d51a194354c6abdb72b076bb642b2b600ce39d89
Rina Andriana <rina@andriana.no>
2022-06-23 12:17:55 +0200

    Fix currency choice length bug for safari

--- src/features/currencyChoice/CurrencyChoice.tsx
+++ src/features/currencyChoice/CurrencyChoice.tsx
@@ -33,12 +33,10 @@ export function CurrencyChoice({ isBustad }: CurrencyChoiceProp) {
           <button onClick={onOpenModal} disabled={isBustad} className="box-content relative w-full bg-Negroni rounded-md flex items-center pl-3 py-2 disabled:cursor-auto cursor-pointer">
             {isBustad ? <BustadIcon className='relative left-1 w-4 mr-2' /> : <img src={chosenOption.img} alt="" className='w-6 max-h-6' />}
               <span className="font-semibold text-l ml-2 mr-1 min-w-[2.6rem]">{currencyDisplay}</span>                          
-            {!isBustad ? <span className="flex items-center pr-2">
               <ChevronDownIcon
-                className="h-6 w-6 text-black"
+                className={`h-6 w-6 ${isBustad ? 'text-Negroni' : 'text-black'} `}
                 aria-hidden="true"
-              /> 
-            </span> : <div className='w-8'></div>}
+              />
           </button>
         </div>
       </div>
