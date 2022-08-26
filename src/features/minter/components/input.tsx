cf9c22580138a20b7b141d9e4071d54ca8ca8afc
Rina Andriana <rina@andriana.no>
2022-08-26 15:32:11 +0200

    Enable minting input again

--- src/features/minter/components/input.tsx
+++ src/features/minter/components/input.tsx
@@ -37,7 +37,7 @@ export function Input({ amount, onChange, insufficientBalance, currencyName, bal
                 </span>
             </div>
             <div className="flex flex-col ml-6 relative">                
-                <input disabled ref={inputRef} value={amount} onChange={e => onChange(e.target.value)} type="text" className="focus:outline-none sm:text-3xl sm:max-w-xs max-w-[9rem]" placeholder="Temporary disabled" />
+            <input ref={inputRef} value={amount} onChange={e => onChange(e.target.value)} type="text" className="focus:outline-none sm:text-3xl sm:max-w-xs max-w-[9rem]" placeholder="0.0" />
                 {insufficientBalance && <span className="absolute top-9 text-red-600 text-xs">insufficient balance </span>}
                 {govTokenToReceive > 0 && <span className="absolute top-7 sm:top-9 text-xs text-gray-700">+ {govTokenToReceive} EIG </span>}
             </div>
