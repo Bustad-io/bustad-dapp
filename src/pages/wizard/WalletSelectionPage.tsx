6417af46d67f417c2366ea1d321a1fbfd4baeebe
Rina Andriana <rina@andriana.no>
2022-11-30 14:16:22 +0100

    Fix store logo and padding on WalletSelectionPage

--- src/pages/wizard/WalletSelectionPage.tsx
+++ src/pages/wizard/WalletSelectionPage.tsx
@@ -3,8 +3,8 @@ import QRCode from "react-qr-code";
 import { useNavigate } from "react-router-dom";
 import { MainBox } from "../../components/MainBox";
 import { WalletSelectorButton } from "../../components/WalletSelectorButton";
-import { ReactComponent as AppStore } from './../../assets/icons/AppStoreLogo.svg';
 import GooglePlay from './../../assets/icons/GooglePlayLogo.png';
+import AppStore from './../../assets/icons/AppStoreLogo.png';
 import { Checkbox } from '../../components/Checkbox';
 import { PrimaryButtonSmall } from "../../components/PrimaryButton";
 import { BrowserView, MobileView, CustomView, isIOS, isMobile } from 'react-device-detect';
@@ -23,10 +23,10 @@ function WalletSelectionPage() {
 
   return (
     <MainBox>
-      <div className="dialog:px-16 my-5">
+      <div className="dialog:px-14 my-4">
         <h1 className="text-white font-bold text-2xl text-center mb-4">How to get a wallet</h1>
         <div className="text-white font-semibold text-lg mb-3">1. Choose a wallet</div>
-        <div className="flex mb-5">
+        <div className="flex mb-7">
           <div className="mr-2">
             <WalletSelectorButton onClick={() => {
               setWalletChoice(COINBASE);
@@ -42,7 +42,7 @@ function WalletSelectionPage() {
         </div>
         <div className="text-white font-semibold text-lg">2. Download and configure {isCoinbase ? 'Coinbase Wallet' : 'MetaMask'}</div>
         {isCoinbase ? (
-          <div className="mb-5">
+          <div className="mb-7">
             <div className="text-white text-xs mb-2">Get the app on your phone</div>
             <BrowserView>
               <QRCode value="http://192.168.128.76:3000/app-store-redirect" size={75} bgColor={'#FF9649'} fgColor={'#FFFFFF'} />
@@ -50,12 +50,12 @@ function WalletSelectionPage() {
             <MobileView>
               <CustomView condition={isIOS}>
                 <a href="https://apps.apple.com/us/app/coinbase-wallet-nfts-crypto/id1278383455">
-                  <AppStore />
+                <img className="w-[125px]" src={AppStore} alt="" />
                 </a>
               </CustomView>
               <CustomView condition={!isIOS}>
                 <a href="https://play.google.com/store/apps/details?id=org.toshi">
-                  <img className="w-[105px]" src={GooglePlay} alt="" />
+                  <img className="w-[125px]" src={GooglePlay} alt="" />
                 </a>
               </CustomView>
             </MobileView>
