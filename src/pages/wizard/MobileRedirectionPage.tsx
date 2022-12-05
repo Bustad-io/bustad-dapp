1e0c5835250bbf924610f3bc5ed357b9e438641d
Rina Andriana <rina@andriana.no>
2022-12-05 12:04:02 +0100

    Fix app store redirect url

--- src/pages/wizard/MobileRedirectionPage.tsx
+++ src/pages/wizard/MobileRedirectionPage.tsx
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
 import { isIOS } from 'react-device-detect';
-import { coinbaseAndroidUrl, coinbaseIosUrl } from '../../config';
 
 function MobileRedirectionPage() {
   useEffect(() => {
     if(isIOS) {
-      window.location.replace(coinbaseIosUrl);
+      window.location.replace('https://apps.apple.com/us/app/coinbase-wallet-nfts-crypto/id1278383455');
     } else {
-      window.location.replace(coinbaseAndroidUrl);
+      window.location.replace('https://play.google.com/store/apps/details?id=org.toshi');
     }
   }, []);
   return (
