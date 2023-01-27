0a06f9542699d160469c369382e415cc270c527b
Rina Andriana <rina@andriana.no>
2023-01-27 09:51:18 +0100

    Remove warnings

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { fetchAccountAsync, fetchGovernanceDistributorShareAsync, selectAccount, selectWalletGovernanceDistributionShare } from "../features/wallet/walletSlice";
 import { useWalletConnection } from '../hooks/walletConnectionHook';
