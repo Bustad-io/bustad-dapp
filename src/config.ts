f6e3b43b65b5434c8bc482a686e9ded4d32f3890
Rina Andriana <rina@andriana.no>
2023-02-01 09:48:46 +0100

    Fix config

--- src/config.ts
+++ src/config.ts
@@ -23,41 +23,8 @@ export const IS_DEV_ENV = process.env.NODE_ENV === 'development';
 export const ANALYTICS_ID_TEST = process.env.REACT_APP_ANALYTICS_ID_TEST ?? "";
 export const ANALYTICS_ID = process.env.REACT_APP_ANALYTICS_ID ?? "";
 
-const DaiDecimal: number = Number(process.env.REACT_APP_DAI_DECIMAL) ?? 0;
-const UsdcDecimal: number = Number(process.env.REACT_APP_USDC_DECIMAL) ?? 0;
-
-const CrowdsaleAddress: string = process.env.REACT_APP_CROWDSALE_ADDRESS ?? "";
-const BustadTokenAddress: string = process.env.REACT_APP_BUSTAD_TOKEN_ADDRESS ?? "";
-const GovDistAddress: string = process.env.REACT_APP_GOVERNANCE_DISTRIBUTOR_ADDRESS ?? "";
-const GovTokenAddress: string = process.env.REACT_APP_GOVERNANCE_TOKEN_ADDRESS ?? "";
-const DaiAddress: string = process.env.REACT_APP_DAI_ADDRESS ?? "";
-const UsdcAddress: string = process.env.REACT_APP_USDC_ADDRESS ?? "";
-
-const CrowdsaleGoerliAddress: string = process.env.REACT_APP_CROWDSALE_ADDRESS_GOERLI ?? "";
-const BustadTokenGoerliAddress: string = process.env.REACT_APP_BUSTAD_TOKEN_ADDRESS_GOERLI ?? "";
-const GovDistGoerliAddress: string = process.env.REACT_APP_GOVERNANCE_DISTRIBUTOR_ADDRESS_GOERLI ?? "";
-const GovTokenGoerliAddress: string = process.env.REACT_APP_GOVERNANCE_TOKEN_ADDRESS_GOERLI ?? "";
-
-const DaiGoerliAddress: string = process.env.REACT_APP_DAI_ADDRESS_GOERLI ?? "";
-const UsdcGoerliAddress: string = process.env.REACT_APP_USDC_ADDRESS_GOERLI ?? "";
-
-const UniswapLpNftGoerliAddress: string = process.env.REACT_APP_UNISWAP_LP_NFT_GOERLI ?? "";
-const UniswapLpNftAddress: string = process.env.REACT_APP_UNISWAP_LP_NFT ?? "";
-const UniswapStakerAddress: string = process.env.REACT_APP_UNISWAP_STAKER ?? "";
-const UniswapStakerGoerliAddress: string = process.env.REACT_APP_UNISWAP_STAKER_GOERLI ?? "";
-
 export const AdminAddress: string = process.env.REACT_APP_ADMIN_ADDRESS ?? "";
-
-export const WETHGoerliAddress: string = process.env.REACT_APP_WETH_TOKEN_GOERLI_ADDRESS ?? "";
-export const WETHAddress: string = process.env.REACT_APP_WETH_TOKEN_ADDRESS ?? "";
-
 export const ServiceBaseUri: string = process.env.REACT_APP_SERVICE_BASE_URI ?? "";
-
-const UniswapLpPoolEthEigAddress: string = process.env.REACT_APP_UNISWAP_LP_POOL_ETH_EIG ?? "";
-const UniswapLpPoolEthEigGoerliAddress: string = process.env.REACT_APP_UNISWAP_LP_POOL_ETH_EIG_GOERLI ?? "";
-const UniswapLpPoolBuscUsdcAddress: string = process.env.REACT_APP_UNISWAP_LP_POOL_BUSC_USDC ?? "";
-const UniswapLpPoolBuscUsdcGoerliAddress: string = process.env.REACT_APP_UNISWAP_LP_POOL_BUSC_USDC_GOERLI ?? "";
-
 
 export function GetContractConfig(network: NetworkTypes): ContractConfig {
 
@@ -65,49 +32,49 @@ export function GetContractConfig(network: NetworkTypes): ContractConfig {
         return {
             Weth: {
                 label: "ETH",
-                address: WETHAddress,                
+                address: process.env.REACT_APP_WETH_TOKEN_ADDRESS!,                
             },
             Usdc: {
                 label: "USDC",
-                address: UsdcAddress,
-                decimal: UsdcDecimal
+                address: process.env.REACT_APP_USDC_ADDRESS!,
+                decimal: 6
             },
             Dai: {
                 label: "DAI",
-                address: DaiAddress,
-                decimal: DaiDecimal
+                address: process.env.REACT_APP_DAI_ADDRESS!,
+                decimal: 18
             },
             Crowdsale: {
                 label: "Crowdsale",
-                address: CrowdsaleAddress
+                address: process.env.REACT_APP_CROWDSALE_ADDRESS!
             },
             BustadCoin: {
                 label: "BUSC",
-                address: BustadTokenAddress
+                address: process.env.REACT_APP_BUSTAD_TOKEN_ADDRESS!
             },
             GovDist: {
                 label: "GovDist",
-                address: GovDistAddress
+                address: process.env.REACT_APP_GOVERNANCE_DISTRIBUTOR_ADDRESS!
             },
             GovToken: {
                 label: "EIG",
-                address: GovTokenAddress
+                address: process.env.REACT_APP_GOVERNANCE_TOKEN_ADDRESS!
             },
             UniswapLpNft: {
                 label: "Uniswap V3 LP NFT",
-                address: UniswapLpNftAddress
+                address: process.env.REACT_APP_UNISWAP_LP_NFT!
             },
             UniswapPoolEthEig: {
                 label: "ETH/EIG 1%",
-                address: UniswapLpPoolEthEigAddress
+                address: process.env.REACT_APP_UNISWAP_LP_POOL_ETH_EIG!
             },
             UniswapPoolBuscUsdc: {
                 label: "BUSC/USDC 0.3%",
-                address: UniswapLpPoolBuscUsdcAddress
+                address: process.env.REACT_APP_UNISWAP_LP_POOL_BUSC_USDC!
             },
             UniswapStaker: {
                 label: "Uniswap V3 Staker",
-                address: UniswapStakerAddress
+                address: process.env.REACT_APP_UNISWAP_STAKER!
             }
         };
     }
@@ -115,49 +82,49 @@ export function GetContractConfig(network: NetworkTypes): ContractConfig {
     return {
         Weth: {
             label: "ETH",
-            address: WETHGoerliAddress,                
+            address: process.env.REACT_APP_WETH_TOKEN_GOERLI_ADDRESS!,                
         },
         Usdc: {
             label: "USDC",
-            address: UsdcGoerliAddress,
-            decimal: UsdcDecimal
+            address: process.env.REACT_APP_USDC_ADDRESS_GOERLI!,
+            decimal: 6
         },
         Dai: {
             label: "DAI",
-            address: DaiGoerliAddress,
-            decimal: DaiDecimal
+            address: process.env.REACT_APP_DAI_ADDRESS_GOERLI!,
+            decimal: 18
         },
         Crowdsale: {
             label: "Crowdsale",
-            address: CrowdsaleGoerliAddress
+            address: process.env.REACT_APP_CROWDSALE_ADDRESS_GOERLI!
         },
         BustadCoin: {
             label: "BUSC",
-            address: BustadTokenGoerliAddress
+            address: process.env.REACT_APP_BUSTAD_TOKEN_ADDRESS_GOERLI!
         },
         GovDist: {
             label: "GovDist",
-            address: GovDistGoerliAddress
+            address: process.env.REACT_APP_GOVERNANCE_DISTRIBUTOR_ADDRESS_GOERLI!
         },
         GovToken: {
             label: "EIG",
-            address: GovTokenGoerliAddress
+            address: process.env.REACT_APP_GOVERNANCE_TOKEN_ADDRESS_GOERLI!
         },
         UniswapLpNft: {
             label: "Uniswap V3 LP NFT",
-            address: UniswapLpNftGoerliAddress
+            address: process.env.REACT_APP_UNISWAP_LP_NFT_GOERLI!
         },
         UniswapPoolEthEig: {
             label: "ETH/EIG 1%",
-            address: UniswapLpPoolEthEigGoerliAddress
+            address: process.env.REACT_APP_UNISWAP_LP_POOL_ETH_EIG_GOERLI!
         },
         UniswapPoolBuscUsdc: {
             label: "BUSC/USDC 0.3%",
-            address: UniswapLpPoolBuscUsdcGoerliAddress
+            address: process.env.REACT_APP_UNISWAP_LP_POOL_BUSC_USDC_GOERLI!
         },
         UniswapStaker: {
             label: "Uniswap V3 Staker",
-            address: UniswapStakerGoerliAddress
+            address: process.env.REACT_APP_UNISWAP_STAKER_GOERLI!
         }
     }
 };
